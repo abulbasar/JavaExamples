852a68087c786a51d71e2917359f6e6d145934e5
Abul Basar <abulbasar@gmail.com>
2021-12-07 10:23:39 +0530

    updated

---
+++ src/main/java/com/example/BankAccount.java
@@ -0,0 +1,88 @@
+package com.example;
+
+public class BankAccount {
+
+    // Member variables
+    // private, public: access modifier
+
+    private Long accountId;
+    private String name;
+    private Double amount;
+    private String status;
+
+    public BankAccount(Long accountId, String name, Double amount, String status){
+        this.setAmount(amount);
+        this.accountId = accountId;
+        this.name = name;
+        this.status = status;
+    }
+
+    public Double getAmount(){
+        return this.amount;
+    }
+
+    private void setAmount(Double value){
+        if(value < 0){
+            throw new RuntimeException("Nagative amount is not allowed");
+        }
+        this.amount = value;
+    }
+
+    // Methods
+    public void withdraw(Double amount){
+        if(amount > this.amount){
+            throw new RuntimeException("Insufficient balance");
+        }
+        this.setAmount(this.amount - amount);
+    }
+
+    public void deposit(Double amount){
+        this.amount = this.amount + amount;
+    }
+
+    public void transfer(BankAccount target, Double amount){
+        if(!this.isActive()){
+            throw new RuntimeException("source account is not active");
+        }
+        if(!target.isActive()){
+            throw new RuntimeException("target account is not active");
+        }
+        this.withdraw(amount);
+        target.deposit(amount);
+    }
+
+    public boolean isActive(){
+        return "Active".equals(status); // status.equals("Active")
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Account(id=%d, name: %s, balance: %f)", accountId, name, amount);
+    }
+
+
+}
---
+++ src/main/java/com/example/Ex02.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import static com.example.common.Utils.println;
+
+public class Ex02 {
+    /*
+    * Define a class
+    * Define member variables
+    * Define methods
+    * Define constructor
+    * Define toString method (override)
+    * Define getter and setter
+    * private and public access modifier
+    * Throw a new exception
+    * How to reach stack trace
+    * */
+    public static void main(String[] args){
+        final BankAccount abulAccount = new BankAccount(1L, "Abul", 100.0, "Active");
+        final BankAccount rahulAccount = new BankAccount(2L, "Rahul", 200.0, "Active");
+        println(String.format("Abul account: %s", abulAccount));
+        println(String.format("Rahul account: %s", rahulAccount));
+        println("Class name: " + BankAccount.class.getName());
+        abulAccount.withdraw(50.0);
+        println(abulAccount);
+    }
+}
