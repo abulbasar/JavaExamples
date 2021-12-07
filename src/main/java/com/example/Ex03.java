3d3252794f6b349305ac12bdec46a271be8b4f7c
Abul Basar <abulbasar@gmail.com>
2021-12-07 13:06:35 +0530

    updated

--- src/main/java/com/example/Ex03.java
+++ src/main/java/com/example/Ex03.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import java.io.Closeable;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Random;
 
-public class Ex03 {
+public class Ex03 implements Closeable {
 
     /*
 
@@ -23,26 +24,59 @@ public class Ex03 {
     *
     *
     * */
-    public static void main(String[] args) throws SQLException {
-        String backend = "RDBMS";
-        AccountService accountService;
 
-        final Random random = new Random();
+    DBSessionService dbSessionService;
+    AccountService accountService;
 
-        if("RDBMS".equals(backend)){
+    private DBSessionService getDbSessionService(){
+        if(dbSessionService == null) {
             String connStr = "jdbc:sqlite:///tmp/sqlite.db";
-            final DBSessionService dbSessionService = new DBSessionService(connStr);
+            dbSessionService =new DBSessionService(connStr);
             dbSessionService.connect();
-            accountService = new RdbmsAccountService(dbSessionService);
-            final BankAccount account = new BankAccount(random.nextLong(), "Abul", 100.0, "Active");
-            accountService.saveAccount(account);
-            final List<BankAccount> bankAccounts = accountService.loadAccounts();
-            final BankAccount bankAccount = accountService.loadAccount(5L);
-            dbSessionService.close();
-        }else if("Mongodb".equals(backend)){
-            accountService = new MongoDbAccountService();
-        }else{
-            throw new RuntimeException("Invalid backend: " + backend);
         }
+        return dbSessionService;
+    }
+
+    private AccountService getAccountService(String backend) throws SQLException {
+        if(accountService == null) {
+            if ("RDBMS".equals(backend)) {
+                final DBSessionService dbSessionService = getDbSessionService();
+                accountService = new RdbmsAccountService(this.dbSessionService);
+            } else if ("Mongodb".equals(backend)) {
+                accountService = new MongoDbAccountService();
+            } else {
+                throw new RuntimeException("Invalid backend: " + backend);
+            }
+        }
+        return accountService;
+    }
+
+    public void start() throws SQLException {
+        String backend = "RDBMS";
+        AccountService accountService = getAccountService(backend);
+        final Random random = new Random();
+        final BankAccount account = new BankAccount(random.nextLong(), "Abul", 100.0, "Active");
+        accountService.saveAccount(account);
+        final List<BankAccount> bankAccounts = accountService.loadAccounts();
+        final BankAccount bankAccount = accountService.loadAccount(5L);
+        System.out.println("Process is complete");
+    }
+
+    @Override
+    public void close(){
+        if(dbSessionService != null){
+            dbSessionService.close();
+        }
+    }
+
+    public Ex03(){
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        final Ex03 app = new Ex03();
+        app.start();
+        app.close();
     }
 }
